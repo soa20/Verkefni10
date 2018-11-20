53d016798d35acea981617589b82e712d906d866
soa20 <soa20@hi.is>
2018-11-20 11:32:09 +0100

    no-console OFF

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     browser: true
   },
   rules: {
-    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+    'no-console': "off",
   },
   plugins: ['import'],
 };
